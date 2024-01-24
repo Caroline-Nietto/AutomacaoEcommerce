7f887b67a43c7d29a5e467fe9e5ebe1a2e8c03f8
Caroline Nietto <niettocaroline@gmail.com>
2024-01-24 11:55:29 -0300

    Revisao steps e feature

--- src/test/java/steps/selectProduct.java
+++ src/test/java/steps/selectProduct.java
@@ -37,71 +37,59 @@ public class selectProduct {
     }
 
 
-
     @Given("I access the giuliana store")
     public void i_access_the_giuliana_store() {
-        driver.get("https://www.giulianaflores.com.br");
+        driver.get("https://www.giulianaflores.com.br/");
     }
-
-
     @When("I click on the product category")
     public void i_click_on_the_product_category() {
         driver.findElement(By.xpath("//h3[@class='title-item' and text()='Buquê de 6 Rosas Vermelhas']")).click();
     }
-
     @Then("I select the product")
     public void i_select_the_product() {
         driver.findElement(By.xpath("//span[@id='ContentSite_lblProductDsName' and @class='jq-product-name'][text()='Buquê de 6 Rosas Vermelhas']")).click();
     }
-
     @Then("I check the product name {string}")
     public void i_check_the_product_name(String productTitle) {
-        assertEquals(driver.findElement(By.id("ContentSite_lblProductDsName")).getText(), productTitle);
+        assertEquals(productTitle, driver.findElement(By.id("ContentSite_lblProductDsName")).getText());
     }
-
     @Then("I check the product price {string}")
     public void i_check_the_product_price(String productPrice) {
         assertEquals(driver.findElement(By.cssSelector("span.precoPor_prod")).getText(), productPrice);
     }
-
     @Then("I click on add to cart")
     public void i_click_on_add_to_cart() {
         driver.findElement(By.id("ContentSite_divBtBuy")).click();
     }
-
     @Then("I fill in the field with the postcode for delivery")
     public void i_fill_in_the_field_with_the_postcode_for_delivery() {
         driver.findElement(By.id("ContentSite_uwcCalendar_txtZip")).sendKeys("14807204");
     }
-
     @When("I click on the ok button to select the postcode")
     public void i_click_on_the_ok_button_to_select_the_postcode() {
         driver.findElement(By.xpath("//span[@class='btn_okcep jSelectZip']")).click();
     }
-
     @Then("I select the delivery period")
     public void i_select_the_delivery_period() {
         driver.findElement(By.id("btConfirmShippingData")).click();
     }
-
     @Then("I click on add to cart after to select the postcode")
     public void i_click_on_add_to_cart_after_to_select_the_postcode() {
-        driver.findElement(By.xpath("//*[@id='ContentSite_lbtBuy']")).click();
+        driver.findElement(By.cssSelector("div.bt_comprar")).click();
     }
-
-    @Then("I validate the page title <pageTitle> in cart")
-    public void i_validate_the_page_title_page_title_in_cart(String pageTitle) {
+    @Then("I validate the page title in cart")
+    public void i_validate_the_page_title_in_cart(String pageTitle) {
         assertEquals(driver.findElement(By.cssSelector("h1.titulo-dept.title-defaut-interna")).getText(), pageTitle);
     }
-
     @Then("I validate the product title {string} in cart")
     public void i_validate_the_product_title_in_cart(String productTitleCart) {
         assertEquals(driver.findElement(By.cssSelector("span.prodBasket_nome")).getText(), productTitleCart);
     }
-
     @Then("I validate the price of the product {string} in cart")
     public void i_validate_the_price_of_the_product_in_cart(String productPriceCart) {
         assertEquals(driver.findElement(By.cssSelector("span.precoPor_basket")).getText(), productPriceCart);
     }
+
+
 
 }
